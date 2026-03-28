33e3cd853da01bcef00dab0dee7ff4956503b84c
Marcelo Henrique <marcelosoares2005@outlook.com>
2026-03-28 19:14:54 -0300

    arquivos modificaados: index.js, package-lock.json, package.json.

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from 'expres';
+
+const host = 'localhost';
+const port = 3000;
+const express = require('express');
+const session = require('express-session');
+const app = express();
